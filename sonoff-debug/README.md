240aeaa9ca91082c2da4a97ac64a4c72c2bd8224
dexter <dexter@dexter.local>
2018-12-01 14:23:17 +0100

    sonoff debug script auto-region; remove sensitive data from output

--- sonoff-debug/README.md
+++ sonoff-debug/README.md
@@ -1,6 +1,12 @@
-Basic script that outputs a json of the current devices you have in your eWeLink account, send this output (post it anywhere and link it to a github issue etc) to me along with the human description of the devices "Sonoff Basic", "Sonoff Dual", "Sonoff 4CH", "Sonoff SC" so I can identify them better etc and I'll do my best to integrate them in this component
+Basic script that outputs a json of the current devices you have in your eWeLink account, send this output (post **full** output anywhere and link it to a github issue etc) to me along with the human description of the devices "Sonoff Basic", "Sonoff Dual", "Sonoff 4CH", "Sonoff SC" so I can identify them better etc and I'll do my best to integrate them in this component.
 
-To run this script just, `cd` to the location of the script and:
+## NOTICE!
+
+Under no circumstances I'm not using this script to capture any emails or passwords and it's better to read/examine its contents before running it! 
+
+### The script tries to remove automatically the sensitive data, but please double check it before posting it - your output might be slightly different!
+
+To run this it just, `cd` to the location of the script and:
 
 `python sonoff-debug.py 'email@used.com' 'password'` > devices.json 
 
@@ -12,10 +18,74 @@ OR
 
 `python sonoff-debug.py -e 'email@used.com' -p 'password'` > devices.json 
 
+## Example of expected output (Sonoff Basic)
 
-##NOTICE 
-
-Under no circumstances I'm not using this script to capture any emails or passwords and it's better to read/examine its contents before running it! 
-
-
+```json
+[
+  {
+    "__v": 0, 
+    "_id": "[hidden]" 
+    "apikey": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", 
+    "brandName": "Sonoff", 
+    "createdAt": "2018-11-18T11:21:58.513Z", 
+    "deviceStatus": "", 
+    "deviceUrl": "", 
+    "deviceid": "[hidden]" 
+    "devicekey": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", 
+    "extra": {
+      "_id": "[hidden]" 
+      "extra": {
+        "apmac": "xx:xx:xx:xx:xx:xx", 
+        "brandId": "58e5f344baeb368720e25469", 
+        "description": "WWJG001219", 
+        "mac": "xx:xx:xx:xx:xx:xx", 
+        "manufacturer": "\u6df1\u5733\u677e\u8bfa\u6280\u672f\u6709\u9650\u516c\u53f8", 
+        "model": "ITA-GZ1-GL", 
+        "modelInfo": "5a2e1ac20cf772f92c342eef", 
+        "ui": "\u5f00\u5173\u6539\u88c5\u6a21\u5757", 
+        "uiid": 14
+      }
+    }, 
+    "group": "", 
+    "groups": [], 
+    "ip": "[hidden]" 
+    "location": "", 
+    "name": "[hidden]", 
+    "offlineTime": "2018-11-29T09:52:16.353Z", 
+    "online": true, 
+    "onlineTime": "2018-11-30T11:25:23.934Z", 
+    "params": {
+      "controlType": 4, 
+      "fwVersion": "2.6.0", 
+      "partnerApikey": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", 
+      "rssi": -59, 
+      "staMac": "xx:xx:xx:xx:xx:xx", 
+      "startup": "off", 
+      "switch": "off", 
+      "timers": [
+        {
+          "at": "40 23 * * 1,2,3,4,5,6,0", 
+          "coolkit_timer_type": "repeat", 
+          "do": {
+            "switch": "off"
+          }, 
+          "enabled": 1, 
+          "mId": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", 
+          "type": "repeat"
+        }
+      ]
+    }, 
+    "productModel": "Basic", 
+    "settings": {
+      "alarmNotify": 1, 
+      "opsHistory": 1, 
+      "opsNotify": 0
+    }, 
+    "sharedTo": [], 
+    "showBrand": true, 
+    "type": "10", 
+    "uiid": 14
+  }
+]
+```
 
--- sonoff-debug/sonoff-debug.py
+++ sonoff-debug/sonoff-debug.py
@@ -1,61 +1,89 @@
 # -*- coding: utf-8 -*-
-import argparse, sys, json, time, random, pprint, base64, requests, hmac, hashlib
+import argparse, sys, json, time, random, pprint, base64, requests, hmac, hashlib, re
+
+# named params	
+if not sys.argv[1].startswith('-') and not sys.argv[2].startswith('-'):
+	email 		= sys.argv[1]
+	password 	= sys.argv[2]
+
+else:
+	parser=argparse.ArgumentParser()
+	parser.add_argument('-e', '--email', 		help='email used for eWeLink app')
+	parser.add_argument('-p', '--password', 	help='password for email/account')
+
+	args=parser.parse_args()
+
+	# positional params
+	if hasattr(args, 'email') and hasattr(args, 'password'):
+		email 		= args.email
+		password 	= args.password
+
+	else:
+		print 'Please read the instructions better!'
+		sys.exit(1)
 
 def gen_nonce(length=8):
 	"""Generate pseudorandom number."""
 	return ''.join([str(random.randint(0, 9)) for i in range(length)])
 
-parser=argparse.ArgumentParser()
+headers = {'Content-Type'  : 'application/json'}
+user_details = {}
+api_region='us'
 
-parser.add_argument('--email', 		help='email used for eWeLink app')
-parser.add_argument('--password', 	help='password for email/account')
+def do_login():
+	global api_region
 
-args=parser.parse_args()
+	decryptedAppSecret = '6Nz4n0xA8s8qdxQf2GqurZj2Fs55FUvM'
+	
+	app_details = {
+		'email'     : email,
+		'password'  : password,
+		'version'   : '6',
+		'ts'        : int(time.time()),
+		'nonce'     : gen_nonce(15),
+		'appid'     : 'oeVkj2lYFGnJu5XUtWisfW4utiN4u9Mq',
+		'imei'      : '01234567-89AB-CDEF-0123-456789ABCDEF',
+		'os'        : 'iOS',
+		'model'     : 'iPhone10,6',
+		'romVersion': '11.1.2',
+		'appVersion': '3.5.3'
+	}
 
-# positional params
-if '--' not in sys.argv[1] and '--' not in sys.argv[2]:
-	email 		= sys.argv[1]
-	password 	= sys.argv[2]
+	hex_dig = hmac.new(decryptedAppSecret, json.dumps(app_details), digestmod=hashlib.sha256).digest()
+	sign = base64.b64encode(hex_dig).decode()
 
-# named params	
-elif hasattr(args, 'email') and hasattr(args, 'password'):
-	email 		= args.email
-	password 	= args.password
+	headers.update({'Authorization' : 'Sign ' + sign})
+	r = requests.post('https://%s-api.coolkit.cc:8080/api/user/login' % api_region, headers=headers, json=app_details)
+	resp = r.json()
+	
+	if 'error' in resp and 'region' in resp and resp['error'] == 301:
+		# re-login using the new localized endpoint
+		api_region = resp['region']
+		do_login()
 
-else:
-	print 'Please read the instructions better!'
-	sys.exit(1)
+	else:
+		global user_details
+		user_details = r.json()
 
-app_details = {
-	'email'     : email,
-	'password'  : password,
-	'version'   : '6',
-	'ts'        : int(time.time()),
-	'nonce'     : gen_nonce(15),
-	'appid'     : 'oeVkj2lYFGnJu5XUtWisfW4utiN4u9Mq',
-	'imei'      : '01234567-89AB-CDEF-0123-456789ABCDEF',
-	'os'        : 'iOS',
-	'model'     : 'iPhone10,6',
-	'romVersion': '11.1.2',
-	'appVersion': '3.5.3'
-}
+def get_devices():
+	headers.update({'Authorization' : 'Bearer ' + user_details['at']})
+	r = requests.get('https://%s-api.coolkit.cc:8080/api/user/device' % api_region, headers=headers)
+	devices = r.json()
 
-decryptedAppSecret = '6Nz4n0xA8s8qdxQf2GqurZj2Fs55FUvM'
+	return json.dumps(devices, indent=2, sort_keys=True)
 
-hex_dig = hmac.new(decryptedAppSecret, json.dumps(app_details), digestmod=hashlib.sha256).digest()
-sign = base64.b64encode(hex_dig).decode()
+def clean_data(data):
+	data = re.sub(r'"phoneNumber": ".*"', '"phoneNumber": "[hidden]"', data)
+	data = re.sub(r'"name": ".*"', '"name": "[hidden]"', data)
+	data = re.sub(r'"ip": ".*",', '"ip": "[hidden]"', data)
+	data = re.sub(r'"deviceid": ".*",', '"deviceid": "[hidden]"', data)
+	data = re.sub(r'"_id": ".*",', '"_id": "[hidden]"', data)
+	data = re.sub(r'"\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2}"', '"xx:xx:xx:xx:xx:xx"', data)
+	data = re.sub(r'"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}"', '"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"', data)
+	return data
 
-headers = {
-	'Authorization' : 'Sign ' + sign,
-	'Content-Type'  : 'application/json'
-}
+if __name__ == "__main__":
+	do_login()
+	devices_json = get_devices()
+	print clean_data(devices_json)
 
-r = requests.post('https://eu-api.coolkit.cc:8080/api/user/login', headers=headers, json=app_details)
-
-user_details = r.json()
-
-headers.update({'Authorization' : 'Bearer ' + user_details['at']})
-r = requests.get('https://eu-api.coolkit.cc:8080/api/user/device', headers=headers)
-devices = r.json()
-
-print json.dumps(devices, indent=2, sort_keys=True)
