abca8f387bd029a5ff794e0917fa33466c9bfb78
Peter Buga <dexter@dexter.local>
2019-04-14 16:00:10 +0200

    light entity automations fix

--- sonoff/light.py
+++ sonoff/light.py
@@ -42,19 +42,30 @@ class SonoffLight(SonoffDevice, LightDevice):
 
         self._name   = device['name']
         self._state  = self.get_state()
+        
+        self._ch01          = [int(device['params']['channel0']), int(device['params']['channel1'])]
+        self._ch234         = [int(device['params']['channel2']), int(device['params']['channel3']), int(device['params']['channel4'])]
 
         # calculate the mode 
-        self._mode          = EFFECT_WARMCOOL
+        if int(device['params']['channel0']) > 0 or int(device['params']['channel1']) > 0:
+            self._mode          = EFFECT_WARMCOOL
+            self._hs_color      = [0, 0]
+        else:
+            self._mode          = EFFECT_COLOR
+            self._hs_color      = color.color_RGB_to_hs(*self._ch234)
 
         # @TODO calculate below values
-        self._hs_color      = [0, 0]
-        self._color_temp    = 350
+        if int(device['params']['channel0']) > 245 and int(device['params']['channel1']) < 10:
+            self._color_temp    = 155
+        elif int(device['params']['channel0']) > 245 and int(device['params']['channel1']) > 245:
+            self._color_temp    = 325
+        elif int(device['params']['channel0']) < 10 and int(device['params']['channel1']) > 245:
+            self._color_temp    = 500
+        else:
+            self._color_temp    = 325 # fallback
 
-        self._ch01          = [255, 255]
-        self._ch234         = [100, 100, 100]
-
-        self._brightness    = 255
-
+        # full brightness all the time till i figure a way to calculate it
+        self._brightness    = 255 
 
     @property
     def name(self):
@@ -96,10 +107,34 @@ class SonoffLight(SonoffDevice, LightDevice):
         """Turn the device on."""
         _LOGGER.debug('Light ON: %s', json.dumps(kwargs) )
 
-        if not len(kwargs): # it's ON/OFF state
-            params = {"state" : "on"}
+        if 'brightness' in kwargs:
+            self._brightness = kwargs['brightness']
 
-        elif 'effect' in kwargs:
+            if self._mode == EFFECT_WARMCOOL:
+
+                ch0 = int(self._ch01[0] * self._brightness / 255)
+                ch1 = int(self._ch01[1] * self._brightness / 255)
+
+                params = {
+                    "channel0": str(ch0), # cool
+                    "channel1": str(ch1), # white
+                    "channel2": "0", # red
+                    "channel3": "0", # green
+                    "channel4": "0" # blue
+                }
+            else: # rgb mode
+                rgb = color.color_hs_to_RGB(*self._hs_color)
+                rgb = [int(x * self._brightness / 255) for x in rgb]
+
+                params = {
+                    "channel0": "0", # cool
+                    "channel1": "0", # white
+                    "channel2": str(rgb[0]), # red
+                    "channel3": str(rgb[1]), # green
+                    "channel4": str(rgb[2]) # blue
+                }
+
+        if 'effect' in kwargs:
             self._mode = kwargs['effect']
 
             if self._mode == EFFECT_WARMCOOL:
@@ -126,15 +161,18 @@ class SonoffLight(SonoffDevice, LightDevice):
                     "channel4": str(rgb[2]) # blue
                 }
 
-        elif 'color_temp' in kwargs:
-            self._color_temp = kwargs['color_temp']
+        if 'color_temp' in kwargs:
+            if self._mode == EFFECT_COLOR:
+                self._mode = EFFECT_WARMCOOL
 
+            self._color_temp = kwargs['color_temp']
             # 347 range value
 
             # middle full cool + warm white
             if kwargs['color_temp'] > 322 and kwargs['color_temp'] < 332:
                 ch0 = 255
                 ch1 = 255
+
             else:
                 if kwargs['color_temp'] <= 322: 
                     color_temp = kwargs['color_temp'] - 153 # default min color temp val
@@ -161,7 +199,10 @@ class SonoffLight(SonoffDevice, LightDevice):
                 "channel4": "0" # blue
             }
 
-        elif 'hs_color' in kwargs:
+        if 'hs_color' in kwargs:
+            if self._mode == EFFECT_WARMCOOL:
+                self._mode = EFFECT_COLOR
+
             self._hs_color = kwargs['hs_color']
             rgb = color.color_hs_to_RGB(*kwargs['hs_color'])
             
@@ -176,32 +217,7 @@ class SonoffLight(SonoffDevice, LightDevice):
                 "channel4": str(rgb[2]) # blue
             }
 
-        elif 'brightness' in kwargs:
-            self._brightness = kwargs['brightness']
-
-            if self._mode == EFFECT_WARMCOOL:
-
-                ch0 = int(self._ch01[0] * self._brightness / 255)
-                ch1 = int(self._ch01[1] * self._brightness / 255)
-
-                params = {
-                    "channel0": str(ch0), # cool
-                    "channel1": str(ch1), # white
-                    "channel2": "0", # red
-                    "channel3": "0", # green
-                    "channel4": "0" # blue
-                }
-            else: # rgb mode
-                rgb = color.color_hs_to_RGB(*self._hs_color)
-                rgb = [int(x * self._brightness / 255) for x in rgb]
-
-                params = {
-                    "channel0": "0", # cool
-                    "channel1": "0", # white
-                    "channel2": str(rgb[0]), # red
-                    "channel3": str(rgb[1]), # green
-                    "channel4": str(rgb[2]) # blue
-                }
+        params.update({"state" : "on"})
 
         _LOGGER.debug('Light params: %s', json.dumps(params) )
 
