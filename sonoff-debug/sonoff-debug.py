8e8afd5625c7236b8d7f880e282ce2b663f9678a
Peter Buga <peter@me.com>
2019-11-17 19:49:32 +0100

    sonoff-debug.py update

--- sonoff-debug/sonoff-debug.py
+++ sonoff-debug/sonoff-debug.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-import argparse, sys, json, time, random, pprint, base64, requests, hmac, hashlib, re
+import argparse, sys, json, time, random, pprint, base64, requests, hmac, hashlib, re, uuid, string
 
-# named params	
+# named params
 if not sys.argv[1].startswith('-') and not sys.argv[2].startswith('-'):
 	username	= sys.argv[1]
 	password 	= sys.argv[2]
@@ -22,29 +22,34 @@ else:
 		print('Please read the instructions better!')
 		sys.exit(1)
 
-def gen_nonce(length=8):
-	"""Generate pseudorandom number."""
-	return ''.join([str(random.randint(0, 9)) for i in range(length)])
-
 headers = {'Content-Type'  : 'application/json'}
 user_details = {}
 api_region='us'
 
+model         = 'iPhone' + random.choice(['6,1', '6,2', '7,1', '7,2', '8,1', '8,2', '8,4', '9,1', '9,2', '9,3', '9,4', '10,1', '10,2', '10,3', '10,4', '10,5', '10,6', '11,2', '11,4', '11,6', '11,8'])
+romVersion    = random.choice([
+	'10.0', '10.0.2', '10.0.3', '10.1', '10.1.1', '10.2', '10.2.1', '10.3', '10.3.1', '10.3.2', '10.3.3', '10.3.4',
+	'11.0', '11.0.1', '11.0.2', '11.0.3', '11.1', '11.1.1', '11.1.2', '11.2', '11.2.1', '11.2.2', '11.2.3', '11.2.4', '11.2.5', '11.2.6', '11.3', '11.3.1', '11.4', '11.4.1',
+	'12.0', '12.0.1', '12.1', '12.1.1', '12.1.2', '12.1.3', '12.1.4', '12.2', '12.3', '12.3.1', '12.3.2', '12.4', '12.4.1', '12.4.2',
+	'13.0', '13.1', '13.1.1', '13.1.2', '13.2'
+])
+appVersion    = random.choice(['3.5.3', '3.5.4', '3.5.6', '3.5.8', '3.5.10', '3.5.12', '3.6.0', '3.6.1', '3.7.0', '3.8.0', '3.9.0', '3.9.1', '3.10.0', '3.11.0'])
+imei          = str(uuid.uuid4())
+
 def do_login():
 	global api_region
 
-	
 	app_details = {
 		'password'  : password,
 		'version'   : '6',
 		'ts'        : int(time.time()),
-		'nonce'     : gen_nonce(15),
+		'nonce'     : ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(8)),
 		'appid'     : 'oeVkj2lYFGnJu5XUtWisfW4utiN4u9Mq',
-		'imei'      : '01234567-89AB-CDEF-0123-456789ABCDEF',
+		'imei'      : imei,
 		'os'        : 'iOS',
-		'model'     : 'iPhone10,6',
-		'romVersion': '11.1.2',
-		'appVersion': '3.5.3'
+		'model'     : model,
+		'romVersion': romVersion,
+		'appVersion': appVersion
 	}
 
 	if '@' not in username:
@@ -66,7 +71,7 @@ def do_login():
 	headers.update({'Authorization' : 'Sign ' + sign})
 	r = requests.post('https://%s-api.coolkit.cc:8080/api/user/login' % api_region, headers=headers, json=app_details)
 	resp = r.json()
-	
+
 	if 'error' in resp and 'region' in resp and resp['error'] == 301:
 		# re-login using the new localized endpoint
 		api_region = resp['region']
@@ -79,7 +84,14 @@ def do_login():
 def get_devices():
 	headers.update({'Authorization' : 'Bearer ' + user_details['at']})
 	r = requests.get('https://%s-api.coolkit.cc:8080/api/user/device?lang=en&apiKey=%s&getTags=1' % \
-		(api_region, user_details['user']['apikey']), 
+		(api_region, user_details['user']['apikey']),
+		headers=headers)
+
+	r = requests.get('https://%s-api.coolkit.cc:8080/api/user/device?lang=en&apiKey=%s&getTags=1&version=6&ts=%s&nonce=%s&appid=oeVkj2lYFGnJu5XUtWisfW4utiN4u9Mq&imei=%s&os=iOS&model=%s&romVersion=%s&appVersion=%s' % (
+			api_region,
+			user_details['user']['apikey'],
+			str(int(time.time())), ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(8)),
+			imei, model, romVersion, appVersion),
 		headers=headers)
 	devices = r.json()
 
@@ -94,6 +106,7 @@ def clean_data(data):
 	data = re.sub(r'"\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2}"', '"xx:xx:xx:xx:xx:xx"', data)
 	data = re.sub(r'"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}"', '"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"', data)
 	data = re.sub(r'"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z"', '"xxxx-xx-xxxxx:xx:xx.xxx"', data)
+
 	return data
 
 if __name__ == "__main__":
