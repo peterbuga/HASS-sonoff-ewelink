dbd9eb401740b8bf30b98d1726ba53c3802bdb65
BeatLink <47278150+BeatLink@users.noreply.github.com>
2020-11-15 02:33:51 -0500

    Fix #263

--- sonoff/__init__.py
+++ sonoff/__init__.py
@@ -23,7 +23,7 @@ CONF_ENTITY_PREFIX  = 'entity_prefix'
 
 DOMAIN              = "sonoff"
 
-REQUIREMENTS        = ['uuid', 'websocket-client==0.54.0']
+REQUIREMENTS        = ['websocket-client==0.54.0']
 
 import websocket
 
